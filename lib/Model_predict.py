70f0e2b1be627adaa953a2e4974c8a9227481b21
ZhiyeGuo <861272076@qq.com>
2022-03-17 17:39:07 -0500

    Modify predict script, add example

--- lib/Model_predict.py
+++ lib/Model_predict.py
@@ -1,7 +1,7 @@
 
 # -*- coding: utf-8 -*-
 """
-Created on Wed June 30 21:47:26 2021
+Created on Thru March 10 21:47:26 2022
 
 @author: Zhiye
 """
@@ -18,6 +18,8 @@ import keras.backend as K
 import tensorflow as tf
 import numpy as np
 import argparse
+from util import *
+from pdb_process import process_pdbfile, get_sequence_from_pdb
 
 class InstanceNormalization(Layer):
     def __init__(self, axis=-1, epsilon=1e-5, **kwargs):
@@ -95,13 +97,13 @@ def get_model_info(model_path):
     model_out = getFileName(model_path, '.json')[0]
     model_weight_out_best = getFileName(model_path, '.h5')
     reject_fea_file = getFileName(model_path, '.txt')[0] 
-    HomoPred = []
+    CDPred = []
     for model_weight in model_weight_out_best:
         with CustomObjectScope({'InstanceNormalization': InstanceNormalization, 'RowNormalization': RowNormalization, 'ColumNormalization': ColumNormalization, 'tf':tf}):
             json_string = open(model_out).read()
             temp_model = model_from_json(json_string)
             temp_model.load_weights(model_weight)
-            HomoPred.append(temp_model)
+            CDPred.append(temp_model)
     accept_list = []
     if not os.path.exists(reject_fea_file):
         print('%s not exists, plese check!'%reject_fea_file)
@@ -113,41 +115,101 @@ def get_model_info(model_path):
                     feature_name = line.strip()
                     feature_name = feature_name[0:]
                     accept_list.append(feature_name)
-    return HomoPred, accept_list
+    return CDPred, accept_list
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Model evalute for DeepComplex')
+    parser.add_argument('-n', '--name', help="protein id, format in A_B", type=str, required=True)
+    parser.add_argument('-p', '--pdb_file_list', help="pdb file or pdb file list", nargs='+', type=str, required=True)
+    parser.add_argument('-a', '--a3m_file', help="mltiple sequence alignment file end in .a3m", type=str, required=True)
+    parser.add_argument('-m', '--model_option', help="model option, i.e: homodimer, heterodimer", type=str, required=False, default='homodimer')
     parser.add_argument('-o', '--out_path', help="output folder", type=str, required=True)
-    parser.add_argument('-m', '--model_path', help="model file in pytorch format, end with '.ckpt'", type=str, required=True)
-    parser.add_argument('-f', '--feature_path', help="folder of feature of test dataset", type=str, required=True)
-    parser.add_argument('-l', '--test_list', help="list file of test dataset", type=str, required=True)
+
 
     args = parser.parse_args()
     name = args.name
+    pdb_file_list = args.pdb_file_list
+    a3m_file = args.a3m_file
     out_path = args.out_path
-    model_path = args.model_path
-    feature_path = args.feature_path
+    model_option = args.model_option
 
-    predict_method = 'realdist_hdist_nointra'
-    HomoPred, accept_list = get_model_info(model_path)
+    chkdirs(out_path)
+    GLOABL_Path = os.path.dirname(os.path.split(os.path.realpath(__file__))[0])
+    print("### Find gloabl path :", GLOABL_Path)
+
+    if model_option == 'homodimer':    
+        model_path = f'{GLOABL_Path}/model/homo/' 
+    elif model_option == 'heterodimer':
+        model_path = f'{GLOABL_Path}/model/hetero/' 
+
+    print("### Find model path :", model_path)
+    CDPred, accept_list = get_model_info(model_path)
     
+    #process pdb
+    pdb_name_list = []
+    length_list = []
+    intra_dist_list = []
+    sequence_list = []
+    complex_length = 0
+    for pdb_file in pdb_file_list:
+        pdb_name = os.path.basename(pdb_file).split('.')[0]
+        pdb_name_list.append(pdb_name)
+        out_file = f'{out_path}/{pdb_name}.pdb'
+        process_pdbfile(pdb_file, out_file)
+        sequence = get_sequence_from_pdb(pdb_file)
+        if len(sequence) > 1:
+            print("Please make sure the pdb file is monomer!")
+        sequence_list.append(sequence[0])
+        length = len(sequence[0])
+        complex_length += length
+        #get distance map from pdb file
+        intra_dist = get_cb_dist_from_pdbfile(out_file, length)
+        intra_dist_list.append(intra_dist)
+
+    print('### Generate features')
+    feature_path = f'{out_path}/feature/'
+    chkdirs(feature_path)
+    os.system(f'cp {a3m_file} {feature_path}')
     a3m_file = f'{feature_path}/{name}.a3m'
-    pssm_file = f'{feature_path}/{name}_pssm.txt'
-    rowatt_file = f'{feature_path}/{name}.npy'
+    aln_file = f'{feature_path}/{name}.aln'
+    os.system("grep -v '^>' %s | sed 's/[a-z]//g' >  %s" % (a3m_file, aln_file))
+
+    fasta_file = f'{feature_path}/{name}.fasta'
+    if model_option == 'homodimer':
+        open(fasta_file, 'w').write(f'>{name}\n{sequence_list[0]}\n')
+    elif model_option == 'heterodimer':
+        open(fasta_file, 'w').write(f'>{name}\n{''.join(sequence_list)}\n')
     ccmpred_file = f'{feature_path}/{name}.mat'
+    if not os.path.exists(ccmpred_file):
+        compute_ccmpred(name, aln_file, save_ccmpred_path=workdir)
+    rowatt_file = f'{feature_path}/{name}.npy'
+    if not os.path.exists(rowatt_file):
+        computerowatt_over1024(name, a3m_file, outdir=workdir, depth=128)
+    pssm_file = f'{feature_path}/{name}_pssm.txt'
+    if not os.path.exists(pssm_file):
+        computepssm(name, fasta_file, workdir, unirefdb)
     pred_dist_file = f'{feature_path}/{name}.dist'
+    if model_option == 'homodimer':
+        np.savetxt(pred_dist_file, intra_dist_list[0], fmt='%.3f')
+    elif model_option == 'heterodimer':
+        intra_dist = np.zeros((complex_length, complex_length))
+        lenA = length_list[0]
+        intra_dist[:lenA, :lenA] = intra_dist_list[0]
+        intra_dist[lenA:, lenA:] = intra_dist_list[1]
+        np.savetxt(pred_dist_file, intra_dist, fmt='%.3f')
   
     selected_list_2D = get2d_feature_by_list(name, accept_list, a3m_file, rowatt_file = rowatt_file, ccmpred_file = ccmpred_file, pssm_file = pssm_file, pred_dist_file_cb=pred_dist_file_cb)
     if type(selected_list_2D) == bool:
         print('Fareture shape error.', selected_list_2D.shape)
         sys.exit(1)
     selected_list_2D = selected_list_2D[np.newaxis,:,:,:]
-    HomoPred_prediction = HomoPred.predict([selected_list_2D], batch_size= 1)
 
-    if predict_method == 'realdist_hdist_nointra':
-        Y_hat_rdist_inter = HomoPred_prediction[0][:,:,:,0:13].sum(axis=-1).squeeze()
-        Y_hat_hdist_inter = HomoPred_prediction[1][:,:,:,0:13].sum(axis=-1).squeeze() #12 A 21
-        # Y_hat_rdist_npy = HomoPred_prediction[0].squeeze()
-        # Y_hat_hdist_npy = HomoPred_prediction[1].squeeze()
-        pred_inter_hdist = f'{out_path}/{name}.htxt' 
-        np.savetxt(pred_inter_hdist, Y_hat_hdist_inter, fmt='%.4f')
+    # for temp in CDPred:
+    #     CDPred_prediction = temp.predict([selected_list_2D], batch_size= 1)
+    #     Y_hat_rdist = CDPred_prediction[0][:,:,:,0:13].sum(axis=-1).squeeze()
+    #     Y_hat_hdist += CDPred_prediction[1][:,:,:,0:13].sum(axis=-1).squeeze()
+    #     if model_option == 'homodimer':
+    #         Y_hat_hdist_inter = Y_hat_hdist
+    #     elif model_option == 'heterodimer':
+    #         value = 
+    #         Y_hat_hdist_inter = Y_hat_hdist_inter[:value,value:]
---
+++ lib/pdb_process.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import argparse
+from Bio import SeqIO
+
+#'TER', 'END'
+def delete_elements(fhandle, element='TER'):
+    for line in fhandle:
+        if line.startswith(element):
+                continue
+        yield line
+    
+def pad_line(line):
+    """Helper function to pad line to 80 characters in case it is shorter"""
+    size_of_line = len(line)
+    if size_of_line < 80:
+        padding = 80 - size_of_line + 1
+        line = line.strip('\n') + ' ' * padding + '\n'
+    return line[:81]  # 80 + newline character
+
+#starting_resid = -1
+def renumber_residues(fhandle, starting_resid):
+    """Resets the residue number column to start from a specific number.
+    """
+    _pad_line = pad_line
+    prev_resid = None  # tracks chain and resid
+    resid = starting_resid - 1  # account for first residue
+    records = ('ATOM', 'HETATM', 'TER', 'ANISOU')
+    for line in fhandle:
+        line = _pad_line(line)
+        if line.startswith(records):
+            line_resuid = line[17:27]
+            if line_resuid != prev_resid:
+                prev_resid = line_resuid
+                resid += 1
+                if resid > 9999:
+                    emsg = 'Cannot set residue number above 9999.\n'
+                    sys.stderr.write(emsg)
+                    sys.exit(1)
+
+            yield line[:22] + str(resid).rjust(4) + line[26:]
+
+        else:
+            yield line
+
+#starting_value = -1
+def renumber_atom_serials(fhandle, starting_value):
+    """Resets the atom serial number column to start from a specific number.
+    """
+
+    # CONECT 1179  746 1184 1195 1203
+    fmt_CONECT = "CONECT{:>5s}{:>5s}{:>5s}{:>5s}{:>5s}" + " " * 49 + "\n"
+    char_ranges = (slice(6, 11), slice(11, 16),
+                   slice(16, 21), slice(21, 26), slice(26, 31))
+
+    serial_equiv = {'': ''}  # store for conect statements
+
+    serial = starting_value
+    records = ('ATOM', 'HETATM')
+    for line in fhandle:
+        if line.startswith(records):
+            serial_equiv[line[6:11].strip()] = serial
+            yield line[:6] + str(serial).rjust(5) + line[11:]
+            serial += 1
+            if serial > 99999:
+                emsg = 'Cannot set atom serial number above 99999.\n'
+                sys.stderr.write(emsg)
+                sys.exit(1)
+
+        elif line.startswith('ANISOU'):
+            # Keep atom id as previous atom
+            yield line[:6] + str(serial - 1).rjust(5) + line[11:]
+
+        elif line.startswith('CONECT'):
+            # 6:11, 11:16, 16:21, 21:26, 26:31
+            serials = [line[cr].strip() for cr in char_ranges]
+
+            # If not found, return default
+            new_serials = [str(serial_equiv.get(s, s)) for s in serials]
+            conect_line = fmt_CONECT.format(*new_serials)
+
+            yield conect_line
+            continue
+
+        elif line.startswith('MODEL'):
+            serial = starting_value
+            yield line
+
+        elif line.startswith('TER'):
+            yield line[:6] + str(serial).rjust(5) + line[11:]
+            serial += 1
+
+        else:
+            yield line
+
+def alter_chain(fhandle, chain_id):
+    """Sets the chain identifier column in all ATOM/HETATM records to a value.
+    """
+    _pad_line = pad_line
+    records = ('ATOM', 'HETATM', 'TER', 'ANISOU')
+    for line in fhandle:
+        if line.startswith(records):
+            line = _pad_line(line)
+            yield line[:21] + chain_id + line[22:]
+        else:
+            yield line
+
+def write_pdb_file(new_pdb, pdb_file):
+    if os.path.exists(pdb_file):
+        os.remove(pdb_file)
+    try:
+        _buffer = []
+        _buffer_size = 5000  # write N lines at a time
+        for lineno, line in enumerate(new_pdb):
+            if not (lineno % _buffer_size):
+                open(pdb_file, 'a').write(''.join(_buffer))
+                _buffer = []
+            _buffer.append(line)
+
+        open(pdb_file, 'a').write(''.join(_buffer))
+    except IOError:
+        # This is here to catch Broken Pipes
+        # for example to use 'head' or 'tail' without
+        # the error message showing up
+        pass
+
+def get_sequence_from_pdb(pdb_file):
+    fh = open(pdb_file, 'r')
+    sequence_list = []
+    for record in SeqIO.parse(fh, 'pdb-atom'):
+        sequence_list.append(str(record.seq))
+    return sequence_list
+
+#process complex_pdb_file 
+def process_pdbfile(in_file, out_file):
+    fhandle = open(in_file, 'r')
+    fhandle = delete_elements(fhandle, 'TER')
+    fhandle = alter_chain(fhandle, 'A')
+    fhandle = renumber_residues(fhandle, 1)
+    fhandle = renumber_atom_serials(fhandle, 1)
+    write_pdb_file(fhandle, out_file)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='pdb process')
+    parser.add_argument('-p', '--pdb_file', help="pdb file", type=str, required=True)
+    parser.add_argument('-o', '--out_file', help="output file", type=str, required=True)
+
+    args = parser.parse_args()  
+    pdb_file = args.pdb_file
+    out_file = args.out_file
+    fhandle = open(pdb_file, 'r')
+
+    fhandle = delete_elements(fhandle, 'TER')
+    fhandle = alter_chain(fhandle, 'A')
+    fhandle = renumber_residues(fhandle, 1)
+    fhandle = renumber_atom_serials(fhandle, 1)
+    write_pdb_file(fhandle, out_file)
